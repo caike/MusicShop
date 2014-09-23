135fb29a7e9961be38ec7d9744637d45801af2b6
Carlos Souza <carloshrsouza@gmail.com>
2014-09-23 18:29:33 -0400

    Add category filter.
    In order to initially load all instruments,
    currentCategory must be undefined.

--- js/client.js
+++ js/client.js
@@ -3,18 +3,28 @@
 
   var app = angular.module('MusicShop', []);
 
-  app.controller("SideBarCtrl", function($scope) {
+  app.controller("MainCtrl", function($scope) {
 
     $scope.categories = ['Guitar', 'Bass', 'Drums'];
 
-  });
-
-  app.controller("MainCtrl", function($scope) {
     $scope.instruments = [
       { name: 'Fender Precision', category: 'Bass' },
       { name: 'Gibson Les Paul Standard', category: 'Guitar' },
       { name: 'TAMA Swingstar ', category: 'Drums' }
     ];
+
+    // TIL if you set this to null, then
+    // filter will not load ANY records.
+    // In order to load ALL records, this needs to be
+    // set to undefined - or simply not set at all.
+    $scope.currentCategory = undefined;
+
+    function setCurrentCategory(category) {
+      $scope.currentCategory = category;
+    }
+
+    $scope.setCurrentCategory = setCurrentCategory;
+
   });
 
 })();
